e3f8cf55bf06fd0622cfe3eee575b35be041b45e
DaffaAfifi <afifidaffa16@gmail.com>
2022-02-14 11:49:37 +0700

    Create Hello.java

---
+++ Minggu 1/Hello.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2;
+
+/**
+ *
+ * @author hp
+ */
+public class Hello {
+    public static void main(String[] args) {
+        
+    //menggunakan print
+        System.out.println("===1===");
+        System.out.print("Hello Word!");
+        System.out.print("Nama saya Daffa\n");
+        
+    //menggunakan println
+        System.out.println("===2===");
+        System.out.println("Hello World!");
+        System.out.println("Nama saya Daffa");
+        
+    //menampilkan isi variabel panjang
+        System.out.println("===3===");
+        int panjang = 10;
+        System.out.println(panjang);
+        
+    //menampilkan kalimat "panjang segi empat" + variabel panjang
+        System.out.println("===4===");
+        System.out.println("panjang segi empat: " + panjang);
+        
+     
+    }
+}
